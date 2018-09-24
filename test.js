1d65185e6f795b9130500d32abefab822c2ba0f7
edwinyoon <ep160101@gmail.com>
2018-09-24 01:27:06 -0400

    Remove unnecessary file

--- test.js
+++
@@ -1,9 +0,0 @@
-const compose = (...fns) => (...args) => fns && fns.map(fn => fn(...args))
-
-const fn1 = ({ re = 1 }) => console.log(re)
-const fn2 = ({ re = 2 }) => console.log(re)
-
-compose(
-  fn1,
-  fn2
-)
