e7dfa2c56a95db676a9d9640a349cca070851a4f
EdwinYoon <ep160101@gmail.com>
2019-06-14 14:11:27 -0400

    Clean up the repo

--- src/config/config.js
+++
@@ -1,1 +0,0 @@
-export const API_AUTH_TOKEN = '2b8fd39c2ce046d0345d62ee514b84f89ef57f0c'
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { API_AUTH_TOKEN } from './config/config'
 import App from './components/App'
 
 const link = new HttpLink({
   uri: 'https://api.github.com/graphql',
   headers: {
-    authorization: `Bearer ${API_AUTH_TOKEN}`,
+    authorization: `Bearer ${process.env.REACT_APP_API_AUTH_TOKEN}`,
   },
 })
 const cache = new InMemoryCache({})
